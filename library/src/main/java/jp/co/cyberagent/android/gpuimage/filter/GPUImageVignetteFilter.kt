7adfd1461959de1e4a09eee61427c8447aabf5d4
Waqas Tahir <wakaztahir@gmail.com>
2023-09-13 13:00:05 +0500

    filters to kotlin more

--- library/src/main/java/jp/co/cyberagent/android/gpuimage/filter/GPUImageVignetteFilter.kt
+++ library/src/main/java/jp/co/cyberagent/android/gpuimage/filter/GPUImageVignetteFilter.kt
@@ -14,98 +14,98 @@
  * limitations under the License.
  */
 
-package jp.co.cyberagent.android.gpuimage.filter;
+package jp.co.cyberagent.android.gpuimage.filter
 
-import android.graphics.PointF;
-import android.opengl.GLES20;
+import android.graphics.PointF
+import android.opengl.GLES20
 
 /**
  * Performs a vignetting effect, fading out the image at the edges
  * x:
  * y: The directional intensity of the vignetting, with a default of x = 0.75, y = 0.5
  */
-public class GPUImageVignetteFilter extends GPUImageFilter {
-    public static final String VIGNETTING_FRAGMENT_SHADER = "" +
-            " uniform sampler2D inputImageTexture;\n" +
-            " varying highp vec2 textureCoordinate;\n" +
-            " \n" +
-            " uniform lowp vec2 vignetteCenter;\n" +
-            " uniform lowp vec3 vignetteColor;\n" +
-            " uniform highp float vignetteStart;\n" +
-            " uniform highp float vignetteEnd;\n" +
-            " \n" +
-            " void main()\n" +
-            " {\n" +
-            "     /*\n" +
-            "     lowp vec3 rgb = texture2D(inputImageTexture, textureCoordinate).rgb;\n" +
-            "     lowp float d = distance(textureCoordinate, vec2(0.5,0.5));\n" +
-            "     rgb *= (1.0 - smoothstep(vignetteStart, vignetteEnd, d));\n" +
-            "     gl_FragColor = vec4(vec3(rgb),1.0);\n" +
-            "      */\n" +
-            "     \n" +
-            "     lowp vec3 rgb = texture2D(inputImageTexture, textureCoordinate).rgb;\n" +
-            "     lowp float d = distance(textureCoordinate, vec2(vignetteCenter.x, vignetteCenter.y));\n" +
-            "     lowp float percent = smoothstep(vignetteStart, vignetteEnd, d);\n" +
-            "     gl_FragColor = vec4(mix(rgb.x, vignetteColor.x, percent), mix(rgb.y, vignetteColor.y, percent), mix(rgb.z, vignetteColor.z, percent), 1.0);\n" +
-            " }";
-
-    private int vignetteCenterLocation;
-    private PointF vignetteCenter;
-    private int vignetteColorLocation;
-    private float[] vignetteColor;
-    private int vignetteStartLocation;
-    private float vignetteStart;
-    private int vignetteEndLocation;
-    private float vignetteEnd;
-
-    public GPUImageVignetteFilter() {
-        this(new PointF(), new float[]{0.0f, 0.0f, 0.0f}, 0.3f, 0.75f);
+class GPUImageVignetteFilter : GPUImageFilter {
+    companion object {
+        const val VIGNETTING_FRAGMENT_SHADER: String = "" +
+                " uniform sampler2D inputImageTexture;\n" +
+                " varying highp vec2 textureCoordinate;\n" +
+                " \n" +
+                " uniform lowp vec2 vignetteCenter;\n" +
+                " uniform lowp vec3 vignetteColor;\n" +
+                " uniform highp float vignetteStart;\n" +
+                " uniform highp float vignetteEnd;\n" +
+                " \n" +
+                " void main()\n" +
+                " {\n" +
+                "     /*\n" +
+                "     lowp vec3 rgb = texture2D(inputImageTexture, textureCoordinate).rgb;\n" +
+                "     lowp float d = distance(textureCoordinate, vec2(0.5,0.5));\n" +
+                "     rgb *= (1.0 - smoothstep(vignetteStart, vignetteEnd, d));\n" +
+                "     gl_FragColor = vec4(vec3(rgb),1.0);\n" +
+                "      */\n" +
+                "     \n" +
+                "     lowp vec3 rgb = texture2D(inputImageTexture, textureCoordinate).rgb;\n" +
+                "     lowp float d = distance(textureCoordinate, vec2(vignetteCenter.x, vignetteCenter.y));\n" +
+                "     lowp float percent = smoothstep(vignetteStart, vignetteEnd, d);\n" +
+                "     gl_FragColor = vec4(mix(rgb.x, vignetteColor.x, percent), mix(rgb.y, vignetteColor.y, percent), mix(rgb.z, vignetteColor.z, percent), 1.0);\n" +
+                " }"
     }
 
-    public GPUImageVignetteFilter(final PointF vignetteCenter, final float[] vignetteColor, final float vignetteStart, final float vignetteEnd) {
-        super(NO_FILTER_VERTEX_SHADER, VIGNETTING_FRAGMENT_SHADER);
-        this.vignetteCenter = vignetteCenter;
-        this.vignetteColor = vignetteColor;
-        this.vignetteStart = vignetteStart;
-        this.vignetteEnd = vignetteEnd;
 
+    private var vignetteCenterLocation: Int = 0
+    private var vignetteCenter: PointF
+    private var vignetteColorLocation: Int = 0
+    private var vignetteColor: FloatArray
+    private var vignetteStartLocation: Int = 0
+    private var vignetteStart: Float
+    private var vignetteEndLocation: Int = 0
+    private var vignetteEnd: Float
+
+    constructor() : this(PointF(), floatArrayOf(0.0f, 0.0f, 0.0f), 0.3f, 0.75f)
+
+    constructor(vignetteCenter: PointF, vignetteColor: FloatArray, vignetteStart: Float, vignetteEnd: Float) : super(
+        NO_FILTER_VERTEX_SHADER,
+        VIGNETTING_FRAGMENT_SHADER
+    ) {
+        this.vignetteCenter = vignetteCenter
+        this.vignetteColor = vignetteColor
+        this.vignetteStart = vignetteStart
+        this.vignetteEnd = vignetteEnd
     }
 
-    @Override
-    public void onInit() {
-        super.onInit();
-        vignetteCenterLocation = GLES20.glGetUniformLocation(getProgram(), "vignetteCenter");
-        vignetteColorLocation = GLES20.glGetUniformLocation(getProgram(), "vignetteColor");
-        vignetteStartLocation = GLES20.glGetUniformLocation(getProgram(), "vignetteStart");
-        vignetteEndLocation = GLES20.glGetUniformLocation(getProgram(), "vignetteEnd");
+    override fun onInit() {
+        super.onInit()
+        vignetteCenterLocation = GLES20.glGetUniformLocation(program, "vignetteCenter")
+        vignetteColorLocation = GLES20.glGetUniformLocation(program, "vignetteColor")
+        vignetteStartLocation = GLES20.glGetUniformLocation(program, "vignetteStart")
+        vignetteEndLocation = GLES20.glGetUniformLocation(program, "vignetteEnd")
     }
 
-    @Override
-    public void onInitialized() {
-        super.onInitialized();
-        setVignetteCenter(vignetteCenter);
-        setVignetteColor(vignetteColor);
-        setVignetteStart(vignetteStart);
-        setVignetteEnd(vignetteEnd);
+    override fun onInitialized() {
+        super.onInitialized()
+        setVignetteCenter(vignetteCenter)
+        setVignetteColor(vignetteColor)
+        setVignetteStart(vignetteStart)
+        setVignetteEnd(vignetteEnd)
     }
 
-    public void setVignetteCenter(final PointF vignetteCenter) {
-        this.vignetteCenter = vignetteCenter;
-        setPoint(vignetteCenterLocation, this.vignetteCenter);
+    fun setVignetteCenter(vignetteCenter: PointF) {
+        this.vignetteCenter = vignetteCenter
+        setPoint(vignetteCenterLocation, this.vignetteCenter)
     }
 
-    public void setVignetteColor(final float[] vignetteColor) {
-        this.vignetteColor = vignetteColor;
-        setFloatVec3(vignetteColorLocation, this.vignetteColor);
+    fun setVignetteColor(vignetteColor: FloatArray) {
+        this.vignetteColor = vignetteColor
+        setFloatVec3(vignetteColorLocation, this.vignetteColor)
     }
 
-    public void setVignetteStart(final float vignetteStart) {
-        this.vignetteStart = vignetteStart;
-        setFloat(vignetteStartLocation, this.vignetteStart);
+    fun setVignetteStart(vignetteStart: Float) {
+        this.vignetteStart = vignetteStart
+        setFloat(vignetteStartLocation, this.vignetteStart)
     }
 
-    public void setVignetteEnd(final float vignetteEnd) {
-        this.vignetteEnd = vignetteEnd;
-        setFloat(vignetteEndLocation, this.vignetteEnd);
+    fun setVignetteEnd(vignetteEnd: Float) {
+        this.vignetteEnd = vignetteEnd
+        setFloat(vignetteEndLocation, this.vignetteEnd)
     }
 }
